a340427cbc5e6b3aa6dcc1863d865d42e52d0534
Makenzie.Moore <makenzie.moore@e-hps.com>
2017-01-21 17:48:38 -0500

    Its so beautiful

--- templates/ach-payment-fields.php
+++ templates/ach-payment-fields.php
@@ -1,76 +1,65 @@
-<?php
 
-  $wrapper = "<div id='HPS_secure_ach'>";
+<div class="ginput_complex<?php echo $class_suffix; ?> ginput_container ginput_container_creditcard" id="<?php echo $field_id; ?>">
+  <div id="HPS_secure_ach">
+    <div class="ss-shield"></div>
+    <!-- account name -->
+    <span class="ginput_full<?php echo $class_suffix; ?>" id="<?php echo $field_id; ?>_5_container">
+      <?php echo $card_icons; ?>
+      <label for="<?php echo $field_id; ?>_5" id="<?php echo $field_id; ?>_5_label" <?php echo $sub_label_class_attribute; ?>>
+        <?php echo $account_name_label; ?>
+        <span class="red">*</span>
+      </label>
 
-$hps_ach_check_holder_field_name = GF_Field_HPSach::HPS_ACH_CHECK_HOLDER_FIELD_NAME;
-$hps_ach_account_field_name = GF_Field_HPSach::HPS_ACH_ACCOUNT_FIELD_NAME;
-$hps_ach_routing_field_name = GF_Field_HPSach::HPS_ACH_ROUTING_FIELD_NAME;
-$hps_ach_type_field_name = GF_Field_HPSach::HPS_ACH_TYPE_FIELD_NAME;
-$hps_ach_check_field_name = GF_Field_HPSach::HPS_ACH_CHECK_FIELD_NAME;
-    $account_name_field =
-        "  <span class='ginput_full{$class_suffix}' id='{$field_id}_5_container'>{$card_icons}
+      <input type="text" name="<?php echo GF_Field_HPSach::HPS_ACH_CHECK_HOLDER_FIELD_NAME; ?>" id="<?php echo $field_id; ?>_5"
+             placeholder="JON DOE" value="<?php echo $account_name_field_input; ?>" <?php echo $disabled_text; ?>/>
+    </span>
+    <!-- account number -->
+    <span class="ginput_full<?php echo $class_suffix; ?>" id="<?php echo $field_id; ?>_6_container" >
+      <label for="<?php echo $field_id; ?>_6" id="<?php echo $field_id; ?>_6_label" <?php echo $sub_label_class_attribute; ?>>
+        <?php echo $account_number_label; ?>
+        <span class="red">*</span>
+      </label>
+      
+      <input type="tel" name="<?php echo GF_Field_HPSach::HPS_ACH_ACCOUNT_FIELD_NAME; ?>" id="<?php echo $field_id; ?>_6"
+             placeholder="• • • • • • • • •" value="<?php echo $account_number_field_input; ?>" <?php echo $disabled_text; ?>/>
+    </span>
+    <!-- routing number -->
+    <span class="ginput_full<?php echo $class_suffix; ?> ginput_cardextras" id="<?php echo $field_id; ?>_7_container">
+      <span id="<?php echo $field_id; ?>_7_accountinfo_left">
+        <label for="<?php echo $field_id; ?>_7" <?php echo $sub_label_class_attribute; ?>>
+          <?php echo $routing_number_label; ?>
+          <span class="red">*</span>
+        </label>
+        
+        <input type="tel" name="<?php echo GF_Field_HPSach::HPS_ACH_ROUTING_FIELD_NAME; ?>" id="<?php echo $field_id; ?>_7"
+               placeholder="• • • • • • • • •" value="<?php echo $routing_number_field_input; ?>" <?php echo $disabled_text; ?>/>
+      </span>
+    </span>
+    <!-- account type -->
+    <span class="ginput_accountinfo_left<?php echo $class_suffix; ?>" id="<?php echo $field_id; ?>_8_accountinfo_left">
+      <label for="<?php echo $field_id; ?>_8" <?php echo $sub_label_class_attribute; ?>>
+        <?php echo $account_type_label; ?>
+        <span class="red">*</span>
+      </label>
 
-              <label for='{$field_id}_5' id='{$field_id}_5_label' {$sub_label_class_attribute}>{$account_name_label}<span class='red'>*</span></label>
+      <select <?php echo $disabled_text; ?>  name="<?php echo GF_Field_HPSach::HPS_ACH_TYPE_FIELD_NAME; ?>" id="<?php echo $field_id; ?>_8">
+        <option value="" disabled selected>Choose One..</option>
+        <option value="0">Checking</option>
+        <option value="1">Savings</option>
+      </select>
+    </span>
+    <!-- check name -->
+    <span class="ginput_accountinfo_right<?php echo $class_suffix; ?>" id="<?php echo $field_id; ?>_9_accountinfo_right">
+      <label for="<?php echo $field_id; ?>_9" <?php echo $sub_label_class_attribute; ?>>
+        <?php echo $check_type_label; ?>
+        <span class="red">*</span>
+      </label>
 
-                  <input type='text' name='{$hps_ach_check_holder_field_name}' id='{$field_id}_5' placeholder='JON DOE' value='{$account_name_field_input}' {$disabled_text}'/>
-
-          </span>";
-
-
-     $account_number_field	=
-          "  <span class='ginput_full{$class_suffix}' id='{$field_id}_6_container' >
-
-                <label for='{$field_id}_6' id='{$field_id}_6_label' {$sub_label_class_attribute}>{$account_number_label}<span class='red'>*</span></label>
-
-                  <input type='tel' name='{$hps_ach_account_field_name}' class='' id='{$field_id}_6' placeholder='000000000' value='{$account_number_field_input}' {$disabled_text}/>
-
-            </span>";
-
-     $routing_number_field 	=
-           "  <span class='ginput_full{$class_suffix} ginput_cardextras' id='{$field_id}_7_container'>
-
-                  <span class='' id='{$field_id}_7_accountinfo_left'>
-
-                        <label for='{$field_id}_7' {$sub_label_class_attribute}>{$routing_number_label}<span class='red'>*</span></label>
-
-                        <input type='tel' name='{$hps_ach_routing_field_name}' id='{$field_id}_7' placeholder='000000000' value='{$routing_number_field_input}' {$disabled_text}/>
-
-                    </span>
-
-              </span>";
-
-     $account_type_field =
-            "  <span class='ginput_accountinfo_left{$class_suffix}' id='{$field_id}_8_accountinfo_left'>
-
-
-                        <label for='{$field_id}_8' {$sub_label_class_attribute}>$account_type_label<span class='red'>*</span></label>
-
-                            <select {$disabled_text}  name='{$hps_ach_type_field_name}' class=''
-                            id='{$field_id}_8'>
-                              <option value='' disabled selected>Choose One..</option>
-                              <option value='0'>Checking</option>
-                              <option value='1'>Savings</option>
-                            </select>
-
-
-               </span>";
-
-    $check_type_field =
-            "  <span class='ginput_accountinfo_right{$class_suffix}' id='{$field_id}_9_accountinfo_right'>
-
-
-                          <label for='{$field_id}_9' {$sub_label_class_attribute}>$check_type_label<span class='red'>*</span></label>
-
-                                <select {$disabled_text} name='{$hps_ach_check_field_name}' class=''
-                                id='{$field_id}_9'>
-                                  <option value='' disabled selected>Choose One..</option>
-                                  <option value='0'>Personal</option>
-                                  <option value='1'>Business</option>
-                                </select>
-
-
-                </span>";
-
-  $wrapper_close = "</div>";
-
-echo  "<div class='ginput_complex{$class_suffix} ginput_container ginput_container_creditcard' id='{$field_id}'>" .$wrapper . $account_name_field . $account_number_field . $routing_number_field . $account_type_field . $check_type_field . $wrapper_close . ' </div>';
+      <select <?php echo $disabled_text; ?> name="<?php echo GF_Field_HPSach::HPS_ACH_CHECK_FIELD_NAME; ?>" id="<?php echo $field_id; ?>_9">
+        <option value="" disabled selected>Choose One..</option>
+        <option value="0">Personal</option>
+        <option value="1">Business</option>
+      </select>
+    </span>
+  </div>
+</div>
